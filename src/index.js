21aeb5869a049d3f599994765c1b3a10e2baaea6
plubojacky <plubojacky@email.com>
2017-09-01 10:49:06 -0700

    Install react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import ContactReducer from './reducers/contact';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+	ContactReducer
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 registerServiceWorker();
